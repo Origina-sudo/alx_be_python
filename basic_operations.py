7245cd89d0efeaa4e44bf49c80f1fa15ed69c963
Benjamin Arthur <arthurba8585@gmail.com>
2024-06-19 18:51:34 +0000

    Addition

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# Assign specific values to the variables
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Print the results
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
