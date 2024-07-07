bda305659841edc912af5faa316db20ec020c60b
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-07 13:36:58 -0700

    ddd

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,17 @@
+# arithmetic_operations.py
+
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation"
+
