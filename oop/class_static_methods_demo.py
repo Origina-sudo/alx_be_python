af8d8666bce4fee90d1095b4194a1a72410a8cf5
Origina-sudo <arthurba8585@gmail.com>
2024-07-28 15:09:24 +0000

    Updates

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,13 @@
+# class_static_methods_demo.py
+
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
