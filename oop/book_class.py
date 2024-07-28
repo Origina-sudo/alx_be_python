d29fff0ae320c95e0e909dd6b0a1358118cdaf48
Origina-sudo <arthurba8585@gmail.com>
2024-07-28 15:01:15 +0000

    added

---
+++ oop/book_class.py
@@ -0,0 +1,16 @@
+# book_class.py
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
