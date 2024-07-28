d8b5c5502a21e61da73a39de06757e6051e89374
Origina-sudo <arthurba8585@gmail.com>
2024-07-28 14:59:44 +0000

    added oop

---
+++ oop/main.py
@@ -0,0 +1,19 @@
+# main.py
+
+from book_class import Book
+
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
+
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
+
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()
